c060076393e6af0ec1a217ba59fdc4acac4f1e5e
Anurag000-rgb <22anurag44@gmail.com>
2021-05-11 20:01:31 +0530

    Building Deployment Serverr

---
+++ src/components/CoinFlipper_Component.js
@@ -0,0 +1,97 @@
+import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+
+import Sdata from './StorageComponents';
+import { random, set } from 'lodash';
+
+
+const Coin = (props) => {
+
+    const [num, setNum] = useState(0);
+
+    let Random = Sdata[Math.floor(Math.random() * Sdata.length)]
+
+
+    
+
+
+    const [head, setHead] = useState(0);
+    const [tail, setTail] = useState(0);
+
+    
+
+    
+
+    return(
+        <Container>
+            <Wrap>
+                <img src= {Random.img} alt= 'coin'/>
+
+                <h1> You Have Clicked {num} Times Head {head} Tail {tail}</h1>
+
+                <button onClick= {() => {
+                    setNum(num+1)
+                    if(Random.id == 0) {
+                        setHead(head+1)
+                    } else {
+                        setTail(tail+1)
+                    }
+                    
+                }}>Spin it 🔥</button>
+            </Wrap>
+        </Container>  
+    )
+}
+
+const Container = styled.section`
+    display: flex;
+    flex-direction: column;
+    inset: 0px;
+    position: relative;
+    align-items: center;
+    justify-content: center;
+    right: auto;
+    left: auto;
+    object-fit: cover;
+`;
+
+const BgImg = styled.div`
+    height: 100%;
+    background-position: top;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-image: url("/images/login-background.jpeg");
+    top: 0;
+    right: 0;
+    left: 0;
+    position: absolute;
+    z-index: -1;
+
+`;
+
+const Wrap = styled.div`
+    
+    img {
+        height: 30vh;
+        width: 200px;
+        border-radius: 50%;
+        margin-right: auto;
+        margin-left: auto;
+        align-items: center;
+        justify-content: center;
+        display: block;
+    }
+
+    button {
+        display: block;
+        margin-right: auto;
+        margin-left: auto;
+        align-items: center;
+        justify-content: center;
+        margin-top: 50px;
+    }
+
+
+`;
+
+export default Coin;
---
+++ src/components/StorageComponents.js
@@ -0,0 +1,11 @@
+const Sdata = [
+    
+        {img: 'https://images-na.ssl-images-amazon.com/images/I/41k2ngUgJ1L.jpg', id: 0},
+
+    
+        {img: 'https://images-na.ssl-images-amazon.com/images/I/41A8Jc5xYVL.jpg', id: 1}
+        
+    
+]
+
+export default Sdata;
